f09cd5f59448c0a237a75701a1b5c59eba22e558
ali63yavari <alimoien.y@gmail.com>
2024-12-21 23:58:43 +0330

    retrack

--- Runtime/Models/BaseGetAllParams.cs
+++
@@ -1,8 +0,0 @@
-namespace DynamicPixels.GameService.Models
-{
-    public abstract class BaseGetAllParams
-    {
-        public int Skip { get; set; }
-        public int Take { get; set; }
-    }
-}
--- Runtime/Services/Synchronise/Repositories/UrlMap.cs
+++
@@ -1,7 +0,0 @@
-namespace DynamicPixels.GameService.Services.Synchronise.Repositories
-{
-    public class UrlMap
-    {
-        public static string GetServerTimeUrl = $"/api/synchronise/time";
-    }
-}
